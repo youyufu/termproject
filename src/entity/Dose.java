c3ea414d622f10adc0c3db2af28da1f93103ce07
alyelshamy <aly.elshamy@mail.utoronto.ca>
2023-11-08 11:46:43 -0500

    updated Dose and Medicine to appropriate attributes

--- src/entity/Dose.java
+++ src/entity/Dose.java
@@ -1,4 +1,16 @@
 package entity;
 
 public class Dose {
+
+    private int size;
+
+    private int inventory;
+
+    private String unit;
+
+
+    public int getDose(){return size;}
+
+    public String getUnit(){return unit;}
+    public int getDosesRemaining(){return inventory - size;}
 }
--- src/entity/Medicine.java
+++ src/entity/Medicine.java
@@ -9,4 +9,7 @@ public class Medicine {
 
    private String description;
 
+
+   }
+
 }
