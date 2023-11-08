7922215977745684dbc2c98a8561bbea2eb85ead
alyelshamy <aly.elshamy@mail.utoronto.ca>
2023-11-08 11:52:39 -0500

    created MedicineFactory with creators

--- src/entity/MedicineFactory.java
+++ src/entity/MedicineFactory.java
@@ -1,4 +1,12 @@
 package entity;
 
 public class MedicineFactory {
+
+    public Medicine createMedicine(String medicine, Dose dose, int[] weeklySchedule, String description){
+        return new Medicine(medicine, dose, weeklySchedule, description);
+    }
+
+    public Dose createDose(int doseSize, int inventory, String unit){
+        return new Dose(doseSize, inventory, unit);
+    }
 }
