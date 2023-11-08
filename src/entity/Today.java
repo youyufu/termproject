b53870b8129e40c4d5b2d6bbf71d2bf9d4f50502
alyelshamy <aly.elshamy@mail.utoronto.ca>
2023-11-08 11:14:41 -0500

    created Today entity, created add, take, undo, using hashmap and its functions

--- src/entity/Today.java
+++ src/entity/Today.java
@@ -1,4 +1,18 @@
 package entity;
 
+import java.util.Map;
+
 public class Today {
+
+    private Map<Medicine, Boolean> todayChecklist;
+
+    public Today(Map<Medicine, Boolean> todayChecklist) {
+        this.todayChecklist = todayChecklist;
+    }
+
+    public void add(Medicine medicine){todayChecklist.put(medicine, Boolean.FALSE);}
+
+    public void take(Medicine medicine){todayChecklist.put(medicine, Boolean.TRUE);}
+
+    public void undo(Medicine medicine){todayChecklist.remove(medicine);}
 }
