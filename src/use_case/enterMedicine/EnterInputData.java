7800367244bbfcf94f1fc87455e06e2829dffffe
You Yu Fu <youyu.fu@mail.utoronto.ca>
2023-11-10 19:46:27 -0500

    Implemented EnterInputData

--- src/use_case/enterMedicine/EnterInputData.java
+++ src/use_case/enterMedicine/EnterInputData.java
@@ -1,4 +1,37 @@
 package use_case.enterMedicine;
 
 public class EnterInputData {
+    final private String medicine;
+    final private Integer doseSize;
+    final private Integer inventory;
+    final private String unit;
+    final private Integer[] day;
+    final private String description;
+
+    public EnterInputData(String medicine,
+                          Integer doseSize,
+                          Integer inventory,
+                          String unit,
+                          Integer[] day,
+                          String description) {
+        this.medicine = medicine;
+        this.doseSize = doseSize;
+        this.inventory = inventory;
+        this.unit = unit;
+        this.day = day;
+        this.description = description;
+    }
+
+    String getMedicine() {return medicine;}
+
+    Integer getDoseSize() {return doseSize;}
+
+    Integer getInventory() {return inventory;}
+
+    String getUnit() {return unit;}
+
+    Integer[] getDay() {return day;}
+
+    String getDescription() {return description;}
+
 }
