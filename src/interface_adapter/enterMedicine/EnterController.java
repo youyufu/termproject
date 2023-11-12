b8c0c453ca2f47b5db65e678f3cc942c68de8010
alyelshamy <aly.elshamy@mail.utoronto.ca>
2023-11-12 15:32:42 -0500

    Created EnterController with the execute method using by delegating the execute method to enterinputboundary using the enterinputdata values.

--- src/interface_adapter/enterMedicine/EnterController.java
+++ src/interface_adapter/enterMedicine/EnterController.java
@@ -1,6 +1,14 @@
 package interface_adapter.enterMedicine;
 
+import use_case.enterMedicine.EnterInputData;
+
 public class EnterController {
+
+    final EnterInputBoundary enterInputBoundary;
+
+    public EnterController(EnterInputBoundary enterInputBoundary1) {
+        this.enterInputBoundary = enterInputBoundary1;
+    }
     public void execute(
             String medicineName,
             String doseSize,
@@ -14,5 +22,8 @@ public class EnterController {
             String friday,
             String saturday,
             String description
-    ) {}
+    ) {EnterInputData enterInputData = new EnterInputData(medicineName, doseSize, doseUnit,
+            doseInventory, sunday, monday, tuesday, wednesday, thursday, friday, saturday, description);
+
+        enterInputBoundary.execute(enterInputData);}
 }
