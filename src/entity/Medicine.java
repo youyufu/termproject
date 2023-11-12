e43a6af190bfe64736e93ba7a44adffcccaa4c4f
Yichen Fan <peteryc.fan@mail.utoronto.ca>
2023-11-11 21:11:12 -0500

    MedicineDAO created, modified Today and Medicine in entity

--- src/entity/Medicine.java
+++ src/entity/Medicine.java
@@ -1,4 +1,13 @@
 package entity;
 
 public class Medicine {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
