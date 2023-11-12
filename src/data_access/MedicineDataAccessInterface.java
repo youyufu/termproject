cc479a360f7398c9c639ac2ee884d38281071a0c
You Yu Fu <youyu.fu@mail.utoronto.ca>
2023-11-12 18:26:54 -0500

    Completed the MedicineDataAccessInterface according to the UML diagram.

--- src/data_access/MedicineDataAccessInterface.java
+++ src/data_access/MedicineDataAccessInterface.java
@@ -1,4 +1,12 @@
 package data_access;
 
+import entity.Medicine;
+import entity.Today;
+
 public interface MedicineDataAccessInterface {
+    public boolean exists(String name);
+    public void saveMedicine(Medicine medicine);
+    public void removeMedicine(String medicine);
+    public void saveToday(Today today);
+    public void updateToday(Medicine medicine);
 }
