3f417bb5eab02f36647d41f34bcc1729f24bc892
You Yu Fu <youyu.fu@mail.utoronto.ca>
2023-11-13 16:19:59 -0500

    added the id attribute to Medicine

--- src/use_case/enterMedicine/EnterInteractor.java
+++ src/use_case/enterMedicine/EnterInteractor.java
@@ -4,6 +4,10 @@ import data_access.MedicineDataAccessInterface;
 import entity.Medicine;
 import entity.Dose;
 import entity.MedicineFactory;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
 
 public class EnterInteractor implements EnterInputBoundary {
     final MedicineDataAccessInterface medicineDataAccessObject;
@@ -24,38 +28,43 @@ public class EnterInteractor implements EnterInputBoundary {
         if (medicineDataAccessObject.exists(name)) {
             enterPresenter.prepareFailView(name + " already exists as a medication");
         }
-        // add the case where a drug interaction is detected
         else {
-            Dose dose = medicineFactory.createDose(enterInputData.getDoseSize(),
-                    enterInputData.getInventory(),
-                    enterInputData.getUnit());
-            Medicine medicine = medicineFactory.createMedicine(enterInputData.getMedicine(),
-                    dose,
-                    enterInputData.getDay(),
-                    enterInputData.getDescription());
-            medicineDataAccessObject.saveMedicine(medicine);
-            EnterOutputData enterOutputData = new EnterOutputData(enterInputData.getMedicine(),
-                    medicine.getDoseString(),
-                    medicine.getInventoryString(),
-                    medicine.getWeeklySchedule()[0],
-                    medicine.getWeeklySchedule()[1],
-                    medicine.getWeeklySchedule()[2],
-                    medicine.getWeeklySchedule()[3],
-                    medicine.getWeeklySchedule()[4],
-                    medicine.getWeeklySchedule()[5],
-                    medicine.getWeeklySchedule()[6],
-                    medicine.getDescription(),
-                    medicine.getDose().getDosesRemaining());
-            enterPresenter.prepareSuccessView(enterOutputData);
-            Integer today = medicineDataAccessObject.getToday().getDay();
-            if (!medicine.getWeeklySchedule()[today].equals(0)) {
-                medicineDataAccessObject.updateToday(medicine);
-                for (int i = 0; i < medicine.getWeeklySchedule()[today]; i++)
-                    enterPresenter.updateChecklistView(enterOutputData);
-            } if (medicine.getDose().getDosesRemaining() < 14) {
-                enterPresenter.updateLowView(enterOutputData);
-            }
+            // API call to get id
+            // API call to drug interaction checker
+            if () {
 
+            }
+            else {
+                Dose dose = medicineFactory.createDose(enterInputData.getDoseSize(),
+                        enterInputData.getInventory(),
+                        enterInputData.getUnit());
+                Medicine medicine = medicineFactory.createMedicine(enterInputData.getMedicine(),
+                        dose,
+                        enterInputData.getDay(),
+                        enterInputData.getDescription());
+                medicineDataAccessObject.saveMedicine(medicine);
+                EnterOutputData enterOutputData = new EnterOutputData(enterInputData.getMedicine(),
+                        medicine.getDoseString(),
+                        medicine.getInventoryString(),
+                        medicine.getWeeklySchedule()[0],
+                        medicine.getWeeklySchedule()[1],
+                        medicine.getWeeklySchedule()[2],
+                        medicine.getWeeklySchedule()[3],
+                        medicine.getWeeklySchedule()[4],
+                        medicine.getWeeklySchedule()[5],
+                        medicine.getWeeklySchedule()[6],
+                        medicine.getDescription(),
+                        medicine.getDose().getDosesRemaining());
+                enterPresenter.prepareSuccessView(enterOutputData);
+                Integer today = medicineDataAccessObject.getToday().getDay();
+                if (!medicine.getWeeklySchedule()[today].equals(0)) {
+                    medicineDataAccessObject.updateToday(medicine);
+                    for (int i = 0; i < medicine.getWeeklySchedule()[today]; i++)
+                        enterPresenter.updateChecklistView(enterOutputData);
+                } if (medicine.getDose().getDosesRemaining() < 14) {
+                    enterPresenter.updateLowView(enterOutputData);
+                }
+            }
         }
     }
 }
