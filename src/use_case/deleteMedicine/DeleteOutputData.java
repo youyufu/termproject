f46e9e8ceb2286b220ea5721bee5759c326f1122
alyelshamy <aly.elshamy@mail.utoronto.ca>
2023-11-13 14:34:45 -0500

    Created the constructor and getMedication method

--- src/use_case/deleteMedicine/DeleteOutputData.java
+++ src/use_case/deleteMedicine/DeleteOutputData.java
@@ -1,4 +1,13 @@
 package use_case.deleteMedicine;
 
 public class DeleteOutputData {
+    private final String medication;
+    public DeleteOutputData(String medication) {
+        this.medication = medication;
+    }
+
+    public String getMedication() {
+        return medication;
+    }
+
 }
