8bf3d26da79ba50116d6efb569fd874f7c9ba4f6
alyelshamy <aly.elshamy@mail.utoronto.ca>
2023-11-13 14:13:48 -0500

    Creating deletePresenter, created prepareSuccessView method

--- src/interface_adapter/deleteMedicine/DeleteController.java
+++ src/interface_adapter/deleteMedicine/DeleteController.java
@@ -1,5 +1,16 @@
 package interface_adapter.deleteMedicine;
 
+import use_case.deleteMedicine.DeleteInputBoundary;
+import use_case.deleteMedicine.DeleteInputData;
+
 public class DeleteController {
-    public void execute(String medicine) {}
+
+    final DeleteInputBoundary deleteInputBoundary;
+
+    public DeleteController(DeleteInputBoundary deleteInputBoundary1){this.deleteInputBoundary = deleteInputBoundary1;}
+    public void execute(String medicine) {
+
+        DeleteInputData deleteInputData = new DeleteInputData(medicine);
+        deleteInputBoundary.execute(deleteInputData);
+    }
 }
