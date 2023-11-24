87de58f472acbbede764a7d9ec505bac5fad81dd
Yichen Fan <peteryc.fan@mail.utoronto.ca>
2023-11-23 22:37:28 -0500

    table was added

--- src/app/TableViewFactory.java
+++ src/app/TableViewFactory.java
@@ -15,15 +15,19 @@ public class TableViewFactory {
         HashMap<String, Medicine> userMedicines = medicineDAO.getUserMedicines();
         TableState tableState = tableViewModel.getState();
         for (Medicine medicine:userMedicines.values()) {
+            CrossUseCaseFactory.map.put(medicine.getName(),Integer.parseInt(medicine.getInventoryString().split(" ")[0]));
+            CrossUseCaseFactory.mapTotal.put(medicine.getName(),Integer.parseInt(medicine.getInventoryString().split(" ")[0]));
             Integer[] weeklySched = medicine.getWeeklySchedule();
             String[] tableData = new String[]{medicine.getName(), medicine.getDoseString(),
                     medicine.getInventoryString(), String.valueOf(weeklySched[0]), String.valueOf(weeklySched[1]),
                     String.valueOf(weeklySched[2]), String.valueOf(weeklySched[3]), String.valueOf(weeklySched[4]),
                     String.valueOf(weeklySched[5]), String.valueOf(weeklySched[6]), medicine.getDescription()};
             tableState.addData(tableData);
-        } tableViewModel.setState(tableState);
+        }
+        tableViewModel.setState(tableState);
         TableView tableView = new TableView(switchViewController, tableViewModel);
         tableViewModel.firePropertyChanged();
+
         return tableView;
     }
 }
