72dc4c367d3f250b26868b7669834d11e75d7ea4
You Yu Fu <youyu.fu@mail.utoronto.ca>
2023-11-24 16:20:38 -0500

    added a check for whether drug interaction exists

--- src/use_case/enterMedicine/EnterInteractor.java
+++ src/use_case/enterMedicine/EnterInteractor.java
@@ -59,62 +59,59 @@ public class EnterInteractor implements EnterInputBoundary {
             try {
                 responseInteraction = HttpClient.newHttpClient().send(requestInteraction, HttpResponse.BodyHandlers.ofString());
                 JSONObject jsonResponseInteraction = new JSONObject(responseInteraction.body());
-                JSONArray fullInteractionTypeGroup = jsonResponseInteraction.getJSONArray("fullInteractionTypeGroup");
-                // TODO check if there is element at index 0
-                JSONArray fullInteractionType = fullInteractionTypeGroup.getJSONObject(0).getJSONArray("fullInteractionType");
-                for (int i = 0; i < fullInteractionType.length(); i++) {
-                    JSONObject eachInteraction = fullInteractionType.getJSONObject(i);
-                    if (eachInteraction.getString("comment").contains(id)) {
-                        // TODO check if there is element at index 0
-                        String description = eachInteraction
-                                .getJSONArray("interactionPair")
-                                .getJSONObject(0).
-                                getString("description");
-                        warnings.add(description);
+                if (jsonResponseInteraction.has("fullInteractionTypeGroup")) {
+                    JSONArray fullInteractionTypeGroup = jsonResponseInteraction.getJSONArray("fullInteractionTypeGroup");
+                    JSONArray fullInteractionType = fullInteractionTypeGroup.getJSONObject(0).getJSONArray("fullInteractionType");
+                    for (int i = 0; i < fullInteractionType.length(); i++) {
+                        JSONObject eachInteraction = fullInteractionType.getJSONObject(i);
+                        if (eachInteraction.getString("comment").contains(id)) {
+                            String description = eachInteraction
+                                    .getJSONArray("interactionPair")
+                                    .getJSONObject(0).
+                                    getString("description");
+                            warnings.add(description);
+                        }
                     }
-                }
-                if (!warnings.isEmpty()) {
-                    StringBuilder popUpMessage = new StringBuilder();
-                    for (String description:warnings) {
-                        popUpMessage.append("Warning - drug interaction detected: ").append(description).append("\n");
+                    if (!warnings.isEmpty()) {
+                        StringBuilder popUpMessage = new StringBuilder();
+                        for (String description:warnings) {
+                            popUpMessage.append("Warning - drug interaction detected: ").append(description).append("\n");
+                        }
+                        enterPresenter.preparePopUp(popUpMessage.toString());
                     }
-                    enterPresenter.preparePopUp(popUpMessage.toString());
                 }
             } catch (IOException e) {
             } catch (InterruptedException e) {
             }
-            if (1 == 2) {}
-            else {
-                Dose dose = medicineFactory.createDose(enterInputData.getDoseSize(),
-                        enterInputData.getInventory(),
-                        enterInputData.getUnit());
-                Medicine medicine = medicineFactory.createMedicine(enterInputData.getMedicine(),
-                        dose,
-                        enterInputData.getDay(),
-                        enterInputData.getDescription(), id);
-                if (medicine.getDose().getDosesRemaining() == 0) {
-                    enterPresenter.preparePopUp("Cannot enter medicine with 0 doses remaining.");
-                } else {
-                    medicineDataAccessObject.saveMedicine(medicine);
-                    EnterOutputData enterOutputData = new EnterOutputData(enterInputData.getMedicine(), medicine.getDoseString(),
-                            medicine.getInventoryString(),
-                            medicine.getWeeklySchedule()[0],
-                            medicine.getWeeklySchedule()[1],
-                            medicine.getWeeklySchedule()[2],
-                            medicine.getWeeklySchedule()[3],
-                            medicine.getWeeklySchedule()[4],
-                            medicine.getWeeklySchedule()[5],
-                            medicine.getWeeklySchedule()[6],
-                            medicine.getDescription(),
-                            medicine.getDose().getDosesRemaining());
-                    enterPresenter.prepareSuccessView(enterOutputData);
-                    Integer today = medicineDataAccessObject.getTodayDay();
-                    for (int i = 0; i < Math.min(medicine.getWeeklySchedule()[today], medicine.getDose().getDosesRemaining()); i++) {
-                        enterPresenter.updateChecklistView(enterOutputData);
-                    }
-                    if (medicine.getDose().getDosesRemaining() < 14) {
-                        enterPresenter.updateLowView(enterOutputData);
-                    }
+            Dose dose = medicineFactory.createDose(enterInputData.getDoseSize(),
+                    enterInputData.getInventory(),
+                    enterInputData.getUnit());
+            Medicine medicine = medicineFactory.createMedicine(enterInputData.getMedicine(),
+                    dose,
+                    enterInputData.getDay(),
+                    enterInputData.getDescription(), id);
+            if (medicine.getDose().getDosesRemaining() == 0) {
+                enterPresenter.preparePopUp("Cannot enter medicine with 0 doses remaining.");
+            } else {
+                medicineDataAccessObject.saveMedicine(medicine);
+                EnterOutputData enterOutputData = new EnterOutputData(enterInputData.getMedicine(), medicine.getDoseString(),
+                        medicine.getInventoryString(),
+                        medicine.getWeeklySchedule()[0],
+                        medicine.getWeeklySchedule()[1],
+                        medicine.getWeeklySchedule()[2],
+                        medicine.getWeeklySchedule()[3],
+                        medicine.getWeeklySchedule()[4],
+                        medicine.getWeeklySchedule()[5],
+                        medicine.getWeeklySchedule()[6],
+                        medicine.getDescription(),
+                        medicine.getDose().getDosesRemaining());
+                enterPresenter.prepareSuccessView(enterOutputData);
+                Integer today = medicineDataAccessObject.getTodayDay();
+                for (int i = 0; i < Math.min(medicine.getWeeklySchedule()[today], medicine.getDose().getDosesRemaining()); i++) {
+                    enterPresenter.updateChecklistView(enterOutputData);
+                }
+                if (medicine.getDose().getDosesRemaining() < 14) {
+                    enterPresenter.updateLowView(enterOutputData);
                 }
             }
         }
