df05b36826f5503bb36dcffccc60a9fd1e67f762
You Yu Fu <youyu.fu@mail.utoronto.ca>
2023-11-27 16:16:06 -0500

    added javadoc for EnterInputBoundary

--- src/use_case/enterMedicine/EnterInputBoundary.java
+++ src/use_case/enterMedicine/EnterInputBoundary.java
@@ -1,5 +1,9 @@
 package use_case.enterMedicine;
 
 public interface EnterInputBoundary {
+    /**
+     * An interface for passing the data the user entered to the interactor.
+     * @param enterInputData
+     */
     void execute(EnterInputData enterInputData);
 }
