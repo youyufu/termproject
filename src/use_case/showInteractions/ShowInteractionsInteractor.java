c31b0a57500b5169940f0611ca91940b393ff00c
You Yu Fu <youyu.fu@mail.utoronto.ca>
2023-11-29 13:33:45 -0500

    created the necessary classes for the new use case.

---
+++ src/use_case/showInteractions/ShowInteractionsInteractor.java
@@ -0,0 +1,16 @@
+package use_case.showInteractions;
+
+public class ShowInteractionsInteractor implements ShowInteractionsInputBoundary {
+    /**
+     * An interactor implementing the logic of showing existing drug interactions.
+     */
+
+    /**
+     * Passes the data to the interactor.
+     * @param showInteractionsInputData the data to be passed
+     */
+    @Override
+    public void execute(ShowInteractionsInputData showInteractionsInputData) {
+
+    }
+}
