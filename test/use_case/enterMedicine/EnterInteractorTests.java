d5950d504e0928fa230cac3f72dd7cfdf2704a69
You Yu Fu <youyu.fu@mail.utoronto.ca>
2023-12-03 18:16:35 -0500

    added a test to EnterInteractorTests for when a medication already exists

--- test/use_case/enterMedicine/EnterInteractorTests.java
+++ test/use_case/enterMedicine/EnterInteractorTests.java
@@ -11,6 +11,7 @@ import data_access.MedicineDataAccessInterface;
 import entity.Medicine;
 import entity.Today;
 import entity.MedicineFactory;
+import org.junit.jupiter.api.Assertions;
 import use_case.enterMedicine.*;
 
 import java.io.File;
@@ -95,5 +96,46 @@ class EnterInteractorTests {
         EnterInputBoundary interactor = new EnterInteractor(
                 userRepository, successPresenter, new MedicineFactory() , new MedicineAPICallsObject());
         interactor.execute(inputData); // This will eventually send Output Data to the successPresenter
+    }
+    @org.junit.jupiter.api.Test
+    void failureMedicineExistsTest() throws IOException {
+        MedicineDataAccessInterface userRepository = new InMemoryDAO(new Today(1), new MedicineFactory());
+        MedicineFactory medicineFactory = new MedicineFactory();
+        Integer[] weeklySchedule = new Integer[]{2,1,3,0,0,1,0};
+        userRepository.saveMedicine(medicineFactory.createMedicine("Oxycontin",
+                2,
+                30,
+                "ml",
+                weeklySchedule,
+                "",
+                "123"));
+        EnterOutputBoundary failurePresenter = new EnterOutputBoundary() {
+            @Override
+            public void prepareSuccessView(EnterOutputData enterOutputData) {
+                fail("Use case should fail when medicine already exists");
+            }
+
+            @Override
+            public void preparePopUp(String message) {
+                assertEquals("Oxycontin already exists as a medication", message);
+            }
+
+            @Override
+            public void updateChecklistView(EnterOutputData enterOutputData) {
+                fail("This view should not be called in case the medication already exists.");
+            }
+
+            @Override
+            public void updateLowView(EnterOutputData enterOutputData) {
+                fail("this view should not be called in case the medication already exists.");
+            }
+        };
+
+        Integer[] myArray = new Integer[]{0,0,0,0,0,0,0};
+        EnterInputData inputData = new EnterInputData("Oxycontin", 3, "tablet",
+                300, myArray , "Do not get addicted" );
+        EnterInputBoundary interactor = new EnterInteractor(
+                userRepository, failurePresenter, new MedicineFactory() , new MedicineAPICallsObject());
+        interactor.execute(inputData);
     }
 }
