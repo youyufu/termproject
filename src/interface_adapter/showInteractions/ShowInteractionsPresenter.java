e58e9a78fffd396c1b256875d96fbdce1009653c
You Yu Fu <youyu.fu@mail.utoronto.ca>
2023-12-03 14:58:40 -0500

    added javadoc for the show interactions use case interface adapters

--- src/interface_adapter/showInteractions/ShowInteractionsPresenter.java
+++ src/interface_adapter/showInteractions/ShowInteractionsPresenter.java
@@ -5,12 +5,23 @@ import interface_adapter.table.TableViewModel;
 import use_case.showInteractions.ShowInteractionsOutputBoundary;
 
 public class ShowInteractionsPresenter implements ShowInteractionsOutputBoundary {
+    /**
+     * A presenter for handling data related to showing medicine interactions.
+     */
     final private TableViewModel tableViewModel;
 
+    /**
+     * Creates a ShowInteractionsPresenter.
+     * @param tableViewModel the view model used to update the medicine table screen.
+     */
     public ShowInteractionsPresenter(TableViewModel tableViewModel) {
         this.tableViewModel = tableViewModel;
     }
 
+    /**
+     * Prepares a pop-up with a message.
+     * @param message the message to be displayed.
+     */
     public void preparePopUp(String message){
         TableState tableState = tableViewModel.getState();
         tableState.setMessage(message);
