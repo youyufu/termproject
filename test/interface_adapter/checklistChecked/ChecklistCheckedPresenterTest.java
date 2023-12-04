ebd40e92845399f037fa5557a63f10b8cd4a3bc6
Yichen Fan <peteryc.fan@mail.utoronto.ca>
2023-12-04 16:26:56 -0500

    ChecklistInteractorTest changed to InMemoryDAO and a mistake was changed to increase lines covered

--- test/interface_adapter/checklistChecked/ChecklistCheckedPresenterTest.java
+++ test/interface_adapter/checklistChecked/ChecklistCheckedPresenterTest.java
@@ -1,20 +1,16 @@
 package interface_adapter.checklistChecked;
 
-import data_access.MedicineDAO;
-import data_access.MedicineDataAccessInterface;
+
 import interface_adapter.table.TableState;
 import interface_adapter.table.TableViewModel;
 import org.junit.jupiter.api.Test;
 import use_case.checklistChecked.ChecklistOutputData;
-
-import java.time.LocalDate;
 
 public class ChecklistCheckedPresenterTest {
     @Test
     void execute() {
         ChecklistViewModel checklistViewModel = new ChecklistViewModel();
         TableViewModel tableViewModel = new TableViewModel();
-        MedicineDataAccessInterface medicineDAO = MedicineDAO.getMedicineDAO(LocalDate.of(2023, 12, 1), "./test.json");
 
         TableState tableState = new TableState();
         tableState.addData(new String[]{"advil", "2 mg", "20 mg", "2", "2", "2", "2", "2", "2", "2", ""});
