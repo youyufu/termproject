63bd697777626a6a4621d1859bfe4ad009bc3f9d
Yichen Fan <peteryc.fan@mail.utoronto.ca>
2023-12-04 14:31:24 -0500

    javadoc added for ChecklistOutputBoundary

--- src/use_case/checklistChecked/ChecklistOutputBoundary.java
+++ src/use_case/checklistChecked/ChecklistOutputBoundary.java
@@ -1,6 +1,21 @@
 package use_case.checklistChecked;
 
 public interface ChecklistOutputBoundary {
+    /**
+     * ChecklistOutputBoundary is an interface in the use case layer.
+     */
+
+    /**
+     * Takes the output data for take method on a medication.
+     *
+     * @param checklistOutputData The output data for checklist.
+     */
     void take(ChecklistOutputData checklistOutputData);
+
+    /**
+     * Takes the output data for 'untake' operation on a medication.
+     *
+     * @param checklistOutputData The output data for checklist.
+     */
     void untake(ChecklistOutputData checklistOutputData);
 }
