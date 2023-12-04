86e659ebe61505e963d4720ce7c0194123f0b234
Yichen Fan <peteryc.fan@mail.utoronto.ca>
2023-12-04 14:16:39 -0500

    javadoc added for ChecklistController

--- src/interface_adapter/checklistChecked/ChecklistController.java
+++ src/interface_adapter/checklistChecked/ChecklistController.java
@@ -4,10 +4,28 @@ import use_case.checklistChecked.ChecklistInputBoundary;
 import use_case.checklistChecked.ChecklistInputData;
 
 public class ChecklistController {
+    /**
+     * ChecklistController is a controller in the interface adapter layer, managing
+     * interactions related to checklist.
+     */
+
     private final ChecklistInputBoundary checklistInputBoundary;
+
+    /**
+     * Constructs a new ChecklistController with a given ChecklistInputBoundary.
+     *
+     * @param checklistInputBoundary The input boundary for checklist.
+     */
     public ChecklistController(ChecklistInputBoundary checklistInputBoundary){
         this.checklistInputBoundary = checklistInputBoundary;
     }
+
+    /**
+     * Executes the checklist operation based on the given medicine name and change value.
+     *
+     * @param medicine The name of the medicine.
+     * @param change The change in the checklist.
+     */
     public void execute(String medicine, Integer change) {
         ChecklistInputData checklistInputData = new ChecklistInputData(medicine, change);
         checklistInputBoundary.execute(checklistInputData);
