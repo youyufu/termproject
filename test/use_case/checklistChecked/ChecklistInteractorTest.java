0921979ad2db8580c092834939e9bcc1811d3137
Daniel Zhang <daniel.zhang9876@gmail.com>
2023-12-05 01:35:34 -0500

    added AppTest and json library xml, changed ChecklistTests to actually assert things, refactored DeleteViewTest to correct test folder location

--- test/use_case/checklistChecked/ChecklistInteractorTest.java
+++ test/use_case/checklistChecked/ChecklistInteractorTest.java
@@ -4,28 +4,31 @@ import data_access.InMemoryDAO;
 import data_access.MedicineDataAccessInterface;
 import entity.Dose;
 import entity.Medicine;
-import interface_adapter.checklistChecked.ChecklistPresenter;
-import interface_adapter.checklistChecked.ChecklistViewModel;
-import interface_adapter.table.TableState;
-import interface_adapter.table.TableViewModel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 public class ChecklistInteractorTest {
     @Test
     void execute() {
-        ChecklistViewModel checklistViewModel = new ChecklistViewModel();
-        TableViewModel tableViewModel = new TableViewModel();
         MedicineDataAccessInterface inMemoryDao = InMemoryDAO.getInMemoryDAO(LocalDate.of(2023, 12, 1));
         Medicine medicine = new Medicine("advil", new Dose(2,2,"pill"), new Integer[]{1,1,1,1,1,1,1} , "advil", "123");
         inMemoryDao.saveMedicine(medicine);
-        TableState tableState = new TableState();
-        tableState.addData(new String[]{"advil", "2 mg", "20 mg", "2", "2", "2", "2", "2", "2", "2", ""});
-        tableViewModel.setState(tableState);
-        tableViewModel.firePropertyChanged();
-        ChecklistPresenter checklistPresenter = new ChecklistPresenter(checklistViewModel,tableViewModel);
-
-        ChecklistInteractor checklistInteractor = new ChecklistInteractor(checklistPresenter,inMemoryDao);
+        ChecklistOutputBoundary checklistOutputBoundary = new ChecklistOutputBoundary() {
+            @Override
+            public void take(ChecklistOutputData checklistOutputData) {
+                Assertions.assertEquals("advil", checklistOutputData.getName());
+                Assertions.assertEquals("1", checklistOutputData.getDosesLeft());
+                Assertions.assertEquals("2 pill", checklistOutputData.getInventory());
+            }
+            @Override
+            public void untake(ChecklistOutputData checklistOutputData) {
+                Assertions.assertEquals("advil", checklistOutputData.getName());
+                Assertions.assertEquals("2", checklistOutputData.getDosesLeft());
+                Assertions.assertEquals("4 pill", checklistOutputData.getInventory());
+            }
+        };
+        ChecklistInteractor checklistInteractor = new ChecklistInteractor(checklistOutputBoundary,inMemoryDao);
         ChecklistInputData checklistInputData = new ChecklistInputData("advil",1);
         checklistInteractor.execute(checklistInputData);
         ChecklistInputData checklistInputData2 = new ChecklistInputData("advil",-1);
