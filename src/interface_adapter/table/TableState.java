53ed64ae0ad93623d75dc21b8e97e28aad600130
Yichen Fan <peteryc.fan@mail.utoronto.ca>
2023-12-06 16:00:13 -0500

    javadoc added for table

--- src/interface_adapter/table/TableState.java
+++ src/interface_adapter/table/TableState.java
@@ -5,9 +5,17 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class TableState {
+    /**
+     * TableState is a class that holds the state of a table view.
+     */
+
     private ArrayList<String[]> data = new ArrayList<>();
     private ArrayList<ArrayList<String>> longest = new ArrayList<>();
     private String message = "";
+
+    /**
+     * Constructs a new instance of TableState.
+     */
     public TableState() {
         String[] headers = {"Medication", "Dose", "Inventory", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Additional Info"};
         for (String header: headers) {
@@ -16,12 +24,30 @@ public class TableState {
             longest.add(list);
         }
     }
+
+    /**
+     * Sets a message related to table.
+     *
+     * @param message The message to be set.
+     */
     public void setMessage(String message) {
         this.message = message;
     }
+
+    /**
+     * Get the current message set for table.
+     *
+     * @return The message for table.
+     */
     public String getMessage() {
         return message;
     }
+
+    /**
+     * Get the current data in table.
+     *
+     * @return An array of the table data.
+     */
     public String[][] getData() {
         String[][] dataArray = new String[data.size()][];
         for (int i = 0; i < data.size(); i++) {
@@ -29,19 +55,37 @@ public class TableState {
             dataArray[i] = array;
         } return dataArray;
     }
+
+    /**
+     * Adds a row of data to the table.
+     *
+     * @param data An array of data to be added.
+     */
     public void addData(String[] data) {
         this.data.add(data);
         for (int i = 0; i < data.length; i++) {
             longest.get(i).add(data[i]);
         }
     }
-    // data in the form of {medication, dose, inventory, Su, M, Tu, W, Th, F, Sa, Description}
+
+    /**
+     * Edits the inventory for a medication in table.
+     *
+     * @param name The name of the medicine.
+     * @param inventory The new inventory of the medicine.
+     */
     public void editInventory(String name, String inventory) {
         int i = 0;
         while (i < data.size() && !data.get(i)[0].equals(name)) {
             i++;
         } data.get(i)[2] = inventory;
     }
+
+    /**
+     * Remove data from table.
+     *
+     * @param name the name of the medicine.
+     */
     public void removeData(String name) {
         int i = 0;
         while (i < data.size() && !data.get(i)[0].equals(name)) {
@@ -51,6 +95,12 @@ public class TableState {
             list.remove(i + 1);
         }
     }
+
+    /**
+     * Get the longest string.
+     *
+     * @return An array of the longest string.
+     */
     public String[] getLongValues() {
         String[] longValues = new String[longest.size()];
         for (int i = 0; i < longest.size(); i++) {
--- src/interface_adapter/table/TableViewModel.java
+++ src/interface_adapter/table/TableViewModel.java
@@ -6,19 +6,54 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class TableViewModel extends ViewModel {
+    /**
+     * TableViewModel is a class that extends ViewModel and manages the state and interactions
+     * for a table view.
+     */
+
     public static final String HEADER_LABEL = "Medication Table";
     public static final String BACK_BUTTON_LABEL = "Back";
     public static final String SHOW_BUTTON_LABEL = "Show All Interactions";
     private TableState tableState = new TableState();
+
+    /**
+     * Constructs a new instance of TableViewModel.
+     */
     public TableViewModel() {}
+
+    /**
+     * Get the current state of the table.
+     *
+     * @return The current table state.
+     */
     public TableState getState() {return tableState;}
+
+    /**
+     * Set the state of the table.
+     *
+     * @param tableState1 The new table state to be set.
+     */
     public void setState(TableState tableState1) {this.tableState = tableState1;}
+
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    /**
+     * Fires a property change event.
+     */
     @Override
     public void firePropertyChanged() {support.firePropertyChange("state", null, tableState);
     }
+
+    /**
+     * Adds a property change listener to this model.
+     *
+     * @param listener The PropertyChangeListener to be added.
+     */
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {support.addPropertyChangeListener(listener);}
 
+    /**
+     * Fires a property change about the need to show a popup.
+     */
     public void firePopUp(){support.firePropertyChange("popup", null, tableState);};
 }
