285e027290043732b3c1ac04d5db0378520706ac
alyelshamy <aly.elshamy@mail.utoronto.ca>
2023-12-06 13:27:40 -0500

    Updated Javadoc

--- src/entity/MedicineFactory.java
+++ src/entity/MedicineFactory.java
@@ -1,6 +1,17 @@
 package entity;
 
 public class MedicineFactory {
+    /**
+     * Constructor class that
+     * @param medicine
+     * @param doseSize
+     * @param inventory
+     * @param unit
+     * @param weeklySchedule
+     * @param description
+     * @param id
+     * @return
+     */
     public Medicine createMedicine(String medicine, Integer doseSize, Integer inventory, String unit, Integer[] weeklySchedule, String description, String id){
         Dose dose = createDose(doseSize, inventory, unit);
         return new Medicine(medicine, dose, weeklySchedule, description, id);
